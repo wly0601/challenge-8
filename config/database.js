9183d7e81c9c9a79d56bcae5d882c8fbf5b0bf04
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2022-06-08 12:32:59 +0700

    intial commit

--- config/database.js
+++ config/database.js
@@ -1,34 +1,41 @@
-const {
-  DB_USER = "",
-  DB_PASSWORD = "",
-  DB_NAME = "bcr",
-  DB_HOST = "127.0.0.1",
-  DB_PORT = "5432",
-} = process.env;
-
 module.exports = {
   development: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres"
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: `${process.env.DB_NAME}_development`,
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
   },
   test: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres"
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: `${process.env.DB_NAME}_test`,
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
   },
   production: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres"
-  }
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: `${process.env.DB_NAME}_production`,
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+  },
 }
