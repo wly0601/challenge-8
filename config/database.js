b3f4497edddb2019e646a35200031ccd7ee04d24
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2022-06-08 13:23:23 +0700

    fix  database.js

--- config/database.js
+++ config/database.js
@@ -2,15 +2,9 @@ module.exports = {
   development: {
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: `${process.env.DB_NAME}_development`,
+      database: `${process.env.DB_NAME}`,
       host: process.env.DB_HOST,
       dialect: "postgres",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
   },
   test: {
       username: process.env.DB_USER,
@@ -28,7 +22,7 @@ module.exports = {
   production: {
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: `${process.env.DB_NAME}_production`,
+      database: `${process.env.DB_NAME}`,
       host: process.env.DB_HOST,
       dialect: "postgres",
       dialectOptions: {
