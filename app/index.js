14964691f94eac492252006c54f15fdd894390e0
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2023-08-09 16:30:28 +0700

    vercel

--- app/index.js
+++ app/index.js
@@ -7,6 +7,8 @@ const router = require("./router")
 const swaggerDocument = require("../docs/swagger.json");
 const { MORGAN_FORMAT } = require("../config/application")
 const app = express();
+const { PORT = 8000 } = process.env;
+
 console.clear();
 
 app.use(morgan(MORGAN_FORMAT));
@@ -14,5 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.get("/documentation.json", (req, res) => res.send(swaggerDocument));
 app.use("/documentation", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+})
 
 module.exports = router.apply(app);
