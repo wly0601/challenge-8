393cf5434386ebef68e4922508b5fd896fe55182
Muhammad Nur Fadli <muhammadnurf10@gmail.com>
2023-08-10 01:46:02 +0700

    vercel

--- app/models/index.js
+++ app/models/index.js
@@ -4,15 +4,20 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+
+let env = 'development';
+if (process.env.NODE_ENV) {
+  env = process.env.NODE_ENV;
+}
+
 const config = require(__dirname + '/../../config/database.js')[env];
 const db = {};
 
 let sequelize;
-if (env !== 'production') {
+try {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(process.env.DB_URL);
+} catch (error) {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
--- config/database.js
+++ config/database.js
@@ -19,7 +19,7 @@ module.exports = {
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: `${process.env.DB_DATABASE}`,
-      url: process.env.DB_URL,
+      host: process.env.DB_HOST,
       dialect: "postgres",
       dialectModule: pg,
       dialectOptions: {
